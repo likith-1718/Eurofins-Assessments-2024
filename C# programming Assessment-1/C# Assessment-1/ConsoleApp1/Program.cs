c9ab7cae75a58c43179b653552aded4de732d83b
LIKITH ABHILASH C <likiab291101@gmail.com>
2024-03-16 11:45:55 +0530

    Add project files.

---
+++ C# programming Assessment-1/C# Assessment-1/ConsoleApp1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Assessment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int overallTotalSales = 0;
+            int overallRetailSales = 0;
+            int overallCorporateSales = 0;
+
+            for (int month = 4; month <= 9; month++)
+            {
+                int daysInMonth = DateTime.DaysInMonth(2024, month);
+                int totalSalesMonth = 0;
+                int retailSalesMonth = 0;
+                int corporateSalesMonth = 0;
+
+                for (int day = 1; day <= daysInMonth; day++)
+                {
+                    int vehiclesSold = VehiclesSold(day);
+
+                    if (day % 5 != 0)
+                    {
+                        totalSalesMonth += vehiclesSold;
+                        overallTotalSales += vehiclesSold;
+                        retailSalesMonth += vehiclesSold;
+                        overallRetailSales += vehiclesSold;
+                    }
+                    else
+                    {
+                        totalSalesMonth += vehiclesSold;
+                        corporateSalesMonth += vehiclesSold;
+                        overallCorporateSales += vehiclesSold;
+                    }
+                }
+
+                Console.WriteLine($"Month: {GetMonthName(month)}, Total Vehicles Sold: {totalSalesMonth}, Retail Sales: {retailSalesMonth}, Corporate Sales: {corporateSalesMonth}");
+            }
+
+            Console.WriteLine($"Overall Total Vehicles Sold between April 1st and September 30th: {overallTotalSales}");
+            Console.WriteLine($"Overall Retail Sales: {overallRetailSales}");
+            Console.WriteLine($"Overall Corporate Sales: {overallCorporateSales}");
+        }
+        static int VehiclesSold(int day)
+        {
+            return (int)Math.Pow((day + 1) / 2, 2);
+        }
+
+        static string GetMonthName(int month)
+        {
+            return new DateTime(2024, month, 1).ToString("MMMM");
+        }
+    }
+}
---
+++ C# programming Assessment-1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[100];
+            Random rnd = new Random();
+            for (int j = 0; j < a.Length; j++)
+            {
+                int d = rnd.Next(100, 999);
+                a[j] = d;
+            }
+            foreach (int j in a)
+            {
+                Console.Write(j + " ");
+            }
+            Console.WriteLine();
+
+            int[] b = new int[] { 10, 23, 17, 12, 6, 9, 11, 19 };
+            int count = 0;
+            for (int j = 1; j < b.Length; j++)
+            {
+                if (isPrime(System.Math.Abs(b[j] - b[j - 1])))
+                {
+                    Console.WriteLine($"{b[j - 1]},{b[j]} ");
+                    count++;
+                }
+            }
+            Console.WriteLine($"Number of such pairs is: {count}");
+
+            int i = 0;
+            if (b[0] < b[1]) Console.Write(b[0] + " ");
+            for (i = 1; i < b.Length; i++)
+            {
+                if (b[i] >= b[i - 1] || b[i] < b[i+1])
+                {
+                    Console.Write(b[i]+" ");
+                }
+                else
+                {
+                    Console.WriteLine();
+                }
+
+            }
+            Console.WriteLine() ;
+        }
+        public static bool isPrime(int a)
+        {
+            if (a == 2) return true;
+            int c = a / 2;
+            for (int i = 2; i < c; i++)
+            {
+                if (a % i == 0) return false;
+            }
+            return true;
+        }
+    }
+}
